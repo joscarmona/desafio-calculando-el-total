d65c08c66cf593c3937741b8e978836d4d0d2243
Jose Carmona <josshue85@gmail.com>
2024-10-20 22:55:01 -0300

    botones + y - operativos, además la visualización del total a pagar mostrándose de forma correcta

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,11 @@
-precio = 400000
+precioUnitario = 400000
 
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+/* Selección del elemento span correspondientea a la clase .precio-unitario */
+precio_unitarioSpan = document.querySelector(".precio-unitario");
+precio_unitarioSpan.innerHTML = (precioUnitario).toLocaleString()
+
+/* Selección del elemento span correspondientea a la clase .cantidad */
+cantidad = document.querySelector('.cantidad')
+
+/* Selección del elemento span correspondientea a la clase .precio-total */
+precio_totalSpan = document.querySelector(".precio-total")
